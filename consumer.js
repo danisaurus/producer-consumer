7c19c88a824412068c27dbaea079d631aac0720e
Dani Dirks <dani.dirks@build.com>
2015-10-17 10:38:37 -0700

    have the two servers speaking to one another

---
+++ consumer.js
@@ -0,0 +1,52 @@
+var http = require('http');
+var connect = require('connect');
+var qs = require('querystring');
+
+
+var server = http.createServer( function(request, response){
+	if (request.method === 'POST') {
+		var body = '';
+		request.on('data', function(data){
+			body += data;
+		})
+		request.on('end', function () {
+			var post = qs.parse(body);
+			console.log('This is the Post', post);
+		});
+	}
+});
+
+server.listen(3000);
+console.log('Server is listening on Port 3000');
+
+
+
+
+// var app = connect();
+
+
+
+// app.use('/', function (request, response, next) {
+// 	console.log('REQ:', request);
+// 	if (request.method === 'POST') {
+// 		
+// 		console.log('I am working');
+// 	}
+// 	request.on('end', function () {
+//         var post = qs.parse(body);
+//         console.log('This is the Post', post);
+//     });
+// 	next();
+// });
+
+
+
+// var server = http.createServer(
+// );
+
+// server.on("listening", function() {
+// 	console.log('hello, i am listening');
+// });
+
+// server.listen('localhost', 8000);
+
--- producer.js
+++ producer.js
@@ -1,36 +1,76 @@
 var http = require('http');
 var connect = require('connect');
-
-var expression = '4+4';
-
-var requestOptions = {
-	hostname: 'localhost',
-	port: 3000,
-	path: '/',
-	method: 'POST',
-	headers: {
-	    'Content-Type': 'application/x-www-form-urlencoded',
-	    'Content-Length': expression.length
-	}
-}
+var qs = require('querystring');
 
 
-var request = http.request(requestOptions, function(res){
-	console.log('STATUS: ' + res.statusCode);
-	console.log('HEADERS: ' + JSON.stringify(res.headers));
-	res.setEncoding('utf8');
-	res.on('data', function (chunk) {
-		console.log('BODY: ' + chunk);
-	});
-	res.on('end', function() {
-		console.log('No more data in response.');
-	})
+var postData = qs.stringify({
+  'msg' : 'Hello World!'
+});
+
+var options = {
+  hostname: 'localhost',
+  port: 3000,
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Length': postData.length
+  }
+};
+
+var req = http.request(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+  });
+  res.on('end', function() {
+    console.log('No more data in response.')
+  })
 });
 
 req.on('error', function(e) {
-	console.log('problem with request: ' + e.message);
+  console.log('problem with request: ' + e.message);
 });
 
+// write data to request body
+req.write(postData);
+req.end();
 
-request.write(expression);
-request.end();
+// var expression =  qs.stringify({
+//   'msg' : '4+4'
+// });
+
+
+// var requestOptions = {
+// 	hostname: 'localhost',
+// 	port: 3000,
+// 	method: 'POST',
+// 	headers: {
+// 	    'Content-Type': 'application/x-www-form-urlencoded',
+// 	    'Content-Length': expression.length,
+// 	    'Connection': 'keep-alive'
+// 	}
+// };
+
+
+// var request = http.request(requestOptions, function(res){
+// 	console.log('STATUS: ' + res.statusCode);
+// 	console.log('HEADERS: ' + JSON.stringify(res.headers));
+// 	res.setEncoding('utf8');
+// 	res.on('data', function (chunk) {
+// 		console.log('BODY: ' + chunk);
+// 	});
+// 	res.on('end', function() {
+// 		console.log('No more data in response.');
+// 	})
+// });
+
+// request.on('error', function(e) {
+// 	console.log('problem with request: ' + e.message);
+// });
+
+
+// request.write(expression);
+// request.end();
+
