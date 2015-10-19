ef530102017e2bdc163361ad9c9f1d54e108378e
Dani Dirks <dani.dirks@build.com>
2015-10-19 16:24:16 -0700

    adding gulp file and mocha and starting to build out tests

--- bin/logger.js
+++ bin/logger.js
@@ -1,24 +1,34 @@
 'use strict';
 var fs = require('fs');
-
-var Logger = function(){};
-
-'use strict';
-var fs = require('fs');
+var assert = require('assert');
 
 var Logger = function(){};
 
 Logger.prototype.logSuccess = function(file, logInfo){
+	
+	assert.equal(typeof (file), 'string', "argument 'file' must be a string");
+	assert.ok(!isNull(logInfo), 'logInfo can not be null');
+	assert.ok(!isNull(logInfo.message), 'logInfo object must contain a message');
+	assert.ok(!isNull(logInfo.timestamp), 'logInfo object must contain a timestamp');
+	assert.ok(!isNull(logInfo.body), 'logInfo object must contain a body');
+
 	var logStatement = logInfo.message + ' at ' + logInfo.timestamp + ' with body ' + logInfo.body + '\n';
 	logEvent(file, logStatement);
 };
 
-Logger.prototype.logError = function(file, event){
-	var logStatement = 'ERROR: There was a ' + event.message + '\n';
+Logger.prototype.logError = function(file, logInfo){
+	assert.equal(typeof (file), 'string', "argument 'file' must be a string");
+	assert.ok(!isNull(logInfo), 'logInfo can not be a null object');
+	assert.ok(!isNull(logInfo.message), 'logInfo object must contain a message');
+
+	var logStatement = 'ERROR: There was a ' + logInfo.message + '\n';
 	logEvent(file, logStatement);
 };
 
 function logEvent(file, logStatement) {
+	assert.equal(typeof (file), 'string', "argument 'file' must be a string");
+	assert.equal(typeof (logStatment), 'string', "argument 'logStatement' must be a string");
+
 	fs.appendFile(file, logStatement, function (err) {
 		if (err) throw err;
 		console.log(logStatement);
