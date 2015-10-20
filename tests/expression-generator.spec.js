89fedaec7d73ab3dc95e155dcc2c1254a01db725
Dani Dirks <dani.dirks@build.com>
2015-10-19 20:00:18 -0700

    adding test for expression generator

--- tests/expression-generator.spec.js
+++ tests/expression-generator.spec.js
@@ -1,3 +1,10 @@
-var assert = require('assert');
+var assert = require('assert'),
+	expressionGenerator = require('../bin/expression-generator.js');
 
 
+describe ('expressionGenerator' function(){
+	it ('should return a string') {
+		assert.ok(typeof(expressionGenerator.expression), 'string');
+	}
+	
+})
